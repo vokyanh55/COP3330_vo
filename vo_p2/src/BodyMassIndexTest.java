ea8473b362b7b502ad60b818bf406e70809cdc2f
vokyanh55 <70238131+vokyanh55@users.noreply.github.com>
2020-09-28 20:55:27 -0400

    Add files via upload

---
+++ vo_p2/src/BodyMassIndexTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BodyMassIndexTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateBMI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCategories() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getScore() {
+    }
+}
